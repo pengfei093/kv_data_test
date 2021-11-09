6bdf3ce48d45d57d7809017234f1f2a785958dc2
pengfei093 <87207094+pengfei093@users.noreply.github.com>
2021-11-09 08:47:04 -0800

    ‘u’

--- kv_database_test.py
+++ kv_database_test.py
@@ -5,8 +5,6 @@ import time
 from utils.kv_database_utils import create_cassandra_cluster
 import signal
 
-# from memory_profiler import profile
-from utils.profiling_utils import timefn
 from utils.util import load_configs_from_files
 
 STRING_LENGTH = 4100
@@ -22,14 +20,37 @@ class KVDatabaseTest:
     # @profile
     def test_insert(self, iter_count=100000):
         t = 0
-        for _ in range(iter_count):
+        file = open('insert_test.txt', 'w')
+        for c in range(iter_count):
             rad_str = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits)
                               for _ in range(STRING_LENGTH))
             t1 = time.time()
             self.current_cassandra_session.execute("INSERT INTO users (name) VALUES (%s)", (rad_str,))
             t2 = time.time()
-            t = t + t2 - t1
+            ct = t2 - t1
+            file.write(str(ct) + '\n')
+            t = t + ct
+            if c % 100 == 1:
+                print(f'iterate {c} times: {t / c}s')
         print(f"insert tooks {t} seconds")
+
+    def test_query(self, iter_count=100000):
+        t = 0
+        file = open('query_test.txt', 'w')
+        for c in range(iter_count):
+            try:
+                t1 = time.time()
+                rows = self.current_cassandra_session.execute('SELECT name FROM users')
+                t2 = time.time()
+                ct = t2 - t1
+                file.write(str(ct) + '\n')
+                t = t + ct
+                if c % 100 == 1:
+                    print(f'iterate {c} times: {t / c}s')
+            except Exception as e:
+                time.sleep(1)
+        print(f"query tooks {t} seconds")
+        file.close()
 
     def start_test(self, set_val, get_val):
         self.current_cassandra_session.set_keyspace('test')
@@ -41,7 +62,7 @@ class KVDatabaseTest:
             );
             """
         )
-        self.test_insert()
+        self.test_query()
 
     def start(self):
         self.start_test(set_val=True, get_val=True)
--- utils/profiling_utils.py
+++ utils/profiling_utils.py
@@ -1,18 +1,18 @@
-import time
-from functools import wraps
-import memory_profiler as mem_profile
-
-
-def timefn(fn):
-    @wraps(fn)
-    def measure_time(*args, **kwargs):
-        t1 = time.time()
-        result = fn(*args, **kwargs)
-        t2 = time.time()
-        print(f"@timefn: {fn.__name__} took {t2 - t1} seconds")
-        return result
-
-    return measure_time
+# import time
+# from functools import wraps
+# import memory_profiler as mem_profile
+#
+#
+# def timefn(fn):
+#     @wraps(fn)
+#     def measure_time(*args, **kwargs):
+#         t1 = time.time()
+#         result = fn(*args, **kwargs)
+#         t2 = time.time()
+#         print(f"@timefn: {fn.__name__} took {t2 - t1} seconds")
+#         return result
+#
+#     return measure_time
 
 
 # def memoryfn(fn):
