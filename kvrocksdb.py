95eb50d87032de1f9e3aec405d3bf78b52889d2f
pengfei093 <87207094+pengfei093@users.noreply.github.com>
2021-12-14 21:58:14 -0800

    peng

--- kvrocksdb.py
+++ kvrocksdb.py
@@ -10,7 +10,7 @@ from utils.util import load_configs_from_files
 
 ITER_COUNT = 500000
 STRING_LENGTH = 4100
-job_id = '_anomalous_country_communication'
+job_id_postfix = '_anomalous_country_communication'
 INSERT_FILE_NAME = 'kvrocksdb_scai_insert.txt'
 QUERY_FILE_NAME = 'kvrocksdb_scai_query.txt'
 JOB_NUM = 1
@@ -20,7 +20,7 @@ class KvRocksdbSCAITest:
     def __init__(self, conf):
         self.conf = conf
         self.current_kvrocksdb_cluster = create_kvrocksdb_cluster(self.conf['kvrocksdb_host'],
-                                                           self.conf['kvrocksdb_port'])
+                                                                  self.conf['kvrocksdb_port'])
 
     def test_insert(self, iter_count=ITER_COUNT):
         t = 0
@@ -28,27 +28,58 @@ class KvRocksdbSCAITest:
                           for _ in range(STRING_LENGTH))
         file = open(INSERT_FILE_NAME, 'w')
         job_ids = []
-        for i in range(JOB_NUM):
-            job_ids.append(str(i) + job_id)
+        for job_index in range(30):
+            job_ids.append(str(job_index) + job_id_postfix)
 
         cur_time = datetime.strptime('26 Sep 2012', '%d %b %Y')
         for c in range(iter_count):
-            for i in range(JOB_NUM):
+            for job_index in range(JOB_NUM):
                 try:
-                    save_time = ((cur_time + c * timedelta(minutes=5)).timestamp())
-                    saved_data = str(save_time) + rad_str
+                    save_time = int((cur_time + c * timedelta(minutes=5)).timestamp())
+                    saved_data = rad_str
+
                     t1 = time.time()
-                    self.current_kvrocksdb_cluster.zadd(job_ids[i], {saved_data: save_time})
+                    self.current_kvrocksdb_cluster.hset(job_ids[job_index], save_time, saved_data)
                     t2 = time.time()
-                    ct = t2 - t1
-                    file.write(str(ct) + '\n')
-                    t = t + ct
+
+                    running_time = t2 - t1
+                    file.write(str(running_time) + '\n')
+                    t = t + running_time
+
                 except Exception as e:
                     print(e)
                     time.sleep(0.01)
             if c % 1000 == 1:
                 print(f'insert {c * JOB_NUM} tooks {t} seconds')
         print(f"insert times tooks {t} seconds")
+
+    # def test_insert(self, iter_count=ITER_COUNT):
+    #     t = 0
+    #     rad_str = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits)
+    #                       for _ in range(STRING_LENGTH))
+    #     file = open(INSERT_FILE_NAME, 'w')
+    #     job_ids = []
+    #     for i in range(JOB_NUM):
+    #         job_ids.append(str(i) + job_id_postfix)
+    #
+    #     cur_time = datetime.strptime('26 Sep 2012', '%d %b %Y')
+    #     for c in range(iter_count):
+    #         for i in range(JOB_NUM):
+    #             try:
+    #                 save_time = int((cur_time + c * timedelta(minutes=5)).timestamp())
+    #                 saved_data = str(save_time) + rad_str
+    #                 t1 = time.time()
+    #                 self.current_kvrocksdb_cluster.zadd(job_ids[i], {saved_data: save_time})
+    #                 t2 = time.time()
+    #                 ct = t2 - t1
+    #                 file.write(str(ct) + '\n')
+    #                 t = t + ct
+    #             except Exception as e:
+    #                 print(e)
+    #                 time.sleep(0.01)
+    #         if c % 1000 == 1:
+    #             print(f'insert {c * JOB_NUM} tooks {t} seconds')
+    #     print(f"insert times tooks {t} seconds")
 
     def test_query(self, iter_count=ITER_COUNT):
         t = 0
@@ -57,7 +88,7 @@ class KvRocksdbSCAITest:
         for c in range(iter_count):
             try:
                 rand_id = str(random.randint(1, JOB_NUM) - 1)
-                job = str(rand_id) + job_id
+                job = str(rand_id) + job_id_postfix
                 start_time = cur_time + c * timedelta(minutes=5)
                 end_time = start_time + timedelta(days=1)
                 start_time = int(start_time.timestamp())
